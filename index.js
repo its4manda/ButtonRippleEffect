9fa548e32c011cfe92dfdafe3d15b7f60802dada
its4manda <its4manda@gmail.com>
2022-11-02 15:02:00 -0300

    inicio do projeto

---
+++ index.js
@@ -0,0 +1,11 @@
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("mouseover", (event)=>{
+
+    const x = (event.pageX - btnEl.offsetLeft);
+    const y = (event.pageX - btnEl.offsetLeft);
+
+    btnEl.style.setProperty("--sPos", x + "px");
+    btnEl.style.setProperty("--yPos", y + "px");
+
+});
